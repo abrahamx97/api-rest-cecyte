d024cd842b83be6df05cb7ee4bacdcfcb3685b18
abrahamx97 <abrahamx97@gmail.com>
2017-12-13 21:31:16 -0600

    funcion para actualizar contrasena

--- src/server/db/queries/alumnos.js
+++ src/server/db/queries/alumnos.js
@@ -1,21 +1,32 @@
 const knex = require('../connection');
+const bcrypt = require('bcrypt')
 
 function getAlumnos() {
-	return knex.select('*').from('valumnosboleta')
+    return knex.select('*').from('valumnosboleta')
 }
 
 function getAlumno(matricula) {
-	return knex.with('inf_alumno', knex.raw(`select * from info_alumno('${matricula}')`)).select('*').from('inf_alumno')
+    return knex.with('inf_alumno', knex.raw(`select * from info_alumno('${matricula}')`)).select('*').from('inf_alumno')
 }
 
-function getCalificacionesAlumno(matricula){
+function getCalificacionesAlumno(matricula) {
     return knex.with('calificaciones', knex.raw(`select * from calificaciones_alumno('${matricula}')`)).select('*').from('calificaciones')
+}
+
+function actualizarContrasena(matricula, alumno) {
+    let contrasena = ''
+    contrasena=bcrypt.hashSync( alumno.contrasena, 10)
+    return knex('control_alumnos')
+        .update({contrasena: contrasena })
+        .where({ matricula: matricula })
+        .returning(['id', 'matricula', 'contrasena']);
 }
 
 
 
 module.exports = {
-	getAlumnos,
+    getAlumnos,
     getAlumno,
-    getCalificacionesAlumno  
+    getCalificacionesAlumno,
+    actualizarContrasena
 };
