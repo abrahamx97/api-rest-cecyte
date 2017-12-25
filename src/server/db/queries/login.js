48af5806f955a2199f07967d42f219b7c05a588c
abrahamx97 <abrahamx97@gmail.com>
2017-12-25 14:31:47 -0600

    funcion para otorgar token

--- src/server/db/queries/login.js
+++ src/server/db/queries/login.js
@@ -1,10 +1,25 @@
-const knex = require('../connection')
 
-function getAlumno(matricula){
-    return knex('control_alumnos')
-    .select('matricula', 'contrasena').where('matricula',matricula)
+const knex = require('../connection')
+const jwt = require('../../jwt/jwt')
+
+
+function getTokenAlumno(matricula){
+    return knex.select('id', 'matricula', 'contrasena').from('control_alumnos')
+    .where({
+        matricula: matricula
+    }).then( (rows)=>{
+        let token = jwt.sign({
+            id: rows[0].id, usuario: rows[0].matricula, contrasena: rows[0].contrasena, tipo: 'alumno'
+        })
+        return knex('control_alumnos')
+        .update({token: token })
+        .where({ matricula: matricula })
+        .returning(['token', 'contrasena']);
+    }).catch((error)=>{    
+        return error
+    })
 }
 
 module.exports = {
-    getAlumno
+    getTokenAlumno
 }
