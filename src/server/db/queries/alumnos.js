e8123bb5082320597d022092b92bb475e90c9c0e
abrahamx97 <abrahamx97@gmail.com>
2017-12-25 14:28:43 -0600

    actualizacion de token y contrasena

--- src/server/db/queries/alumnos.js
+++ src/server/db/queries/alumnos.js
@@ -1,32 +1,44 @@
 const knex = require('../connection');
 const bcrypt = require('bcrypt')
+const jwt = require('../../jwt/jwt')
 
 function getAlumnos() {
-    return knex.select('*').from('valumnosboleta')
+    return knex.with('inf_alumnos', knex.raw(`select * from info_alumnos()`)).select('*').from('inf_alumnos')
 }
 
-function getAlumno(matricula) {
-    return knex.with('inf_alumno', knex.raw(`select * from info_alumno('${matricula}')`)).select('*').from('inf_alumno')
+function getAlumno(matricula, token) {
+    return knex.with('inf_alumno', knex.raw(`select * from info_alumno('${matricula}', '${token}')`)).select('*').from('inf_alumno')
 }
 
-function getCalificacionesAlumno(matricula) {
-    return knex.with('calificaciones', knex.raw(`select * from calificaciones_alumno('${matricula}')`)).select('*').from('calificaciones')
+function getCalificacionesAlumno(matricula, token) {
+    return knex.with('calificaciones', knex.raw(`select * from calificaciones_alumno('${matricula}', '${token}')`)).select('*').from('calificaciones')
 }
 
-function actualizarContrasena(matricula, alumno) {
+function actualizarContrasena(matricula, alumno, token) {
     let contrasena = ''
-    contrasena=bcrypt.hashSync( alumno.contrasena, 10)
-    return knex('control_alumnos')
-        .update({contrasena: contrasena })
+    contrasena = bcrypt.hashSync(alumno.contrasena, 10)
+    return knex.select('id', 'matricula').from('control_alumnos')
         .where({ matricula: matricula })
-        .returning(['id', 'matricula', 'contrasena']);
+        .then((rows) => {
+            let token = jwt.sign({
+                id: rows[0].id, usuario: rows[0].matricula, contrasena: contrasena, tipo: 'alumno'
+            })
+            return knex('control_alumnos')
+            .update({
+                contrasena: contrasena,
+                token: token
+            })
+            .where({matricula: matricula})
+            .returning(['token'])
+        }).catch((error)=>{
+            return error
+        })
 }
-
 
 
 module.exports = {
     getAlumnos,
     getAlumno,
     getCalificacionesAlumno,
-    actualizarContrasena
+    actualizarContrasena,
 };
